88545a99655cac331347c4ff1df63fb2f4c7adcd
Victor Lupascu <sinajeen@gmail.com>
2021-06-09 17:20:33 -0500

    add a warning with a link if there is more than 30 issues in the list

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector('#issues-container');
+var limitWarningEl = document.querySelector('#limit-warning');
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -7,6 +8,10 @@ var getRepoIssues = function (repo) {
         if (response.ok) {
             response.json().then(function (data) {
                 displayIssues(data);
+                // check if api has paginated issues
+                if (response.headers.get('Link')) {
+                    displayWarning(repo);
+                }
             });
         } else {
             alert('there was a problem with your request');
@@ -49,6 +54,18 @@ var displayIssues = function (issues) {
         // append to the main page
         issueContainerEl.appendChild(issueEl);
     }
+};
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = 'To see more than 30 issues, visit ';
+    var linkEl = document.createElement('a');
+    linkEl.textContent = 'see more Issues on GitHub.com';
+    linkEl.setAttribute('href', 'https://github.com/' + repo + '/issues');
+    linkEl.setAttribute('target', 'blank');
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
 }
 
 getRepoIssues('facebook/react');
